20245b980b268d4b44f1d78485252fff3f7f7250
onekx <1095716578@qq.com>
2020-05-14 20:13:09 +0800

    添加主页导航

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Message from './Message';
+import User from './User';
+
+const Tab = createBottomTabNavigator()
 
 export default function Main() {
     return(
-        <View>
-            <Text>主界面</Text>
-        </View>
+        <Tab.Navigator initialRouteName="消息">
+            <Tab.Screen name="消息" component={Message} />
+            <Tab.Screen name="我" component={User} />
+        </Tab.Navigator>
     )
 }
