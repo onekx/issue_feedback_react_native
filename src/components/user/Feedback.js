8cfea4a5a6bd1426b861e59573dc66a6d0d4cfa5
onekx <1095716578@qq.com>
2020-05-21 16:50:16 +0800

    个人中心布局

---
+++ src/components/user/Feedback.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function Feedback() {
+    return(
+        <View>
+            <Text style={{color: '#B2B8CA',paddingLeft: 10, paddingTop: 10}}>还没有反馈</Text>
+        </View>
+    )
+}
