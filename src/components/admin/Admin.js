ac6b944672d4677e5a980f0e6b890ac83929578f
kongxin <43380516+onekx@users.noreply.github.com>
2020-05-26 19:29:03 +0800

    完善后台管理 (#9)

--- src/components/admin/Admin.js
+++ src/components/admin/Admin.js
@@ -1,49 +1,15 @@
-import React, { Component } from 'react'
-import { Container, Header, Body, Button, Segment, Content, Text } from 'native-base'
+import React from 'react'
+import { createDrawerNavigator } from '@react-navigation/drawer'
+import ManageFeedback from './ManageFeedback'
 import CreateProduct from './CreateProduct'
-import Feedbacks from './Feedbacks'
 
-export default class Admin extends Component {
-    state = {
-        activeComponent: 'feedbacks'
-    }
+const Drawer = createDrawerNavigator()
 
-    selectComponent = (activeComponent) => () => this.setState({activeComponent})
-
-    renderComponent = () => {
-        const { activeComponent } = this.state
-        if (activeComponent === 'feedbacks')
-            return <Feedbacks />
-        else 
-            return <CreateProduct />       
-    }
-
-    render() {
-        const { activeComponent } = this.state
-        return(
-            <Container>
-                <Header hasSegment>
-                <Body>
-                    <Segment>
-                        <Button first 
-                            active={activeComponent === 'feedbacks'}
-                            onPress={this.selectComponent('feedbacks')}
-                        >
-                            <Text>最近反馈</Text>
-                        </Button>
-                        <Button last
-                            active={activeComponent === 'product'}
-                            onPress={this.selectComponent('product')}
-                        >
-                            <Text>创建产品</Text>
-                        </Button>
-                    </Segment>
-                </Body>
-                </Header>
-                <Content>
-                    {this.renderComponent()}
-                </Content>
-            </Container>
-        )
-    }
+export default function Admin() {   
+    return(
+        <Drawer.Navigator initialRouteName="最近反馈">
+            <Drawer.Screen name="最近反馈" component={ManageFeedback} />
+            <Drawer.Screen name="创建产品" component={CreateProduct} />
+        </Drawer.Navigator>
+    )
 }
