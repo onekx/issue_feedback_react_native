91aa1988d247ac2735fff81d2a457727b9afd98f
kongxin <43380516+onekx@users.noreply.github.com>
2020-05-27 22:54:37 +0800

    主页展示产品反馈 (#14)

--- src/components/user/Feedback.js
+++ src/components/user/Feedback.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { Component } from 'react'
+import { Card, CardItem, Thumbnail, Text, Left, Body, Right, Icon } from 'native-base'
+import Request from '../../api/Request'
 
-export default function Feedback() {
-    return(
-        <View>
-            <Text style={{color: '#B2B8CA',paddingLeft: 10, paddingTop: 10}}>还没有反馈</Text>
-        </View>
-    )
+export default class Feedback extends Component {
+    state = {
+        nickName: ''
+    }
+
+    getNickName = () => {
+        const { userId } = this.props
+        Request(`/profile/${userId}`)
+          .then(res => this.setState({
+              nickName: res.result.nickname
+          }))
+    }
+
+    render() {
+        const { description, time } = this.props
+        const { nickName } = this.state
+        const uri = "https://facebook.github.io/react-native/docs/assets/favicon.png"
+        if (nickName === '') this.getNickName()
+        return(
+            <Card>
+                <CardItem header>
+                    <Left>
+                        <Thumbnail square small source={{uri: uri}} />
+                        <Text>{nickName}</Text>
+                    </Left>
+                    <Right>
+                        <Text note>{time}</Text>
+                    </Right>
+                </CardItem>
+                <CardItem button onPress={() => alert("This is Feedback Body")}>
+                    <Body>
+                        <Text numberOfLines={1}>
+                            {description}
+                        </Text>
+                    </Body>
+                </CardItem>
+                <CardItem footer>
+                    <Left>
+                        <Icon type="AntDesign" name="like1" style={{fontSize: 20, color: 'red'}} />
+                        <Text>100</Text>
+                        <Icon type="AntDesign" name="dislike1" style={{fontSize: 20, color: 'gray', marginLeft: 10}} />
+                        <Text>50</Text>
+                    </Left>
+                </CardItem>
+            </Card>
+        )
+    }
 }
--- src/components/user/Home.js
+++ src/components/user/Home.js
@@ -1,38 +1,70 @@
-import React from 'react'
-import { Container, Content, List, ListItem, Thumbnail, Text, Left, Body, Right, Fab, Icon } from 'native-base'
-import { Image, View, PixelRatio } from 'react-native'
+import React, { Component } from 'react'
+import { Container, Content, Fab, Icon } from 'native-base'
+import { Image, DeviceEventEmitter } from 'react-native'
+import Feedback from './Feedback'
+import Request from '../../api/Request'
 
-export default function Home({ navigation }) {
-    console.disableYellowBox = true
-    return(
-        <Container>
-            <Content>
-                <Image source={ require('../../assets/images/home.jpg') } style={{ width: 345, height: 125, borderRadius: 10, margin: 7 }} />
-                <View style={{ height: 1/PixelRatio.get(), backgroundColor:'#808080' }}/>
-                
-                <List>
-                  <ListItem avatar>
-                    <Left>
-                      <Thumbnail source={{ uri: 'https://facebook.github.io/react-native/docs/assets/favicon.png' }} />
-                    </Left>
-                    <Body>
-                      <Text>哇哇哈哈</Text>
-                      <Text note>下载的视频打不开打打不开打不开打不打不开打不开打不开打不开打不开</Text>
-                    </Body>
-                    <Right>
-                      <Text note>3:43 pm</Text>
-                    </Right>
-                  </ListItem>
-              </List>
-          
-            </Content>
-            <Fab
-              style={{ backgroundColor: '#5067FF' }}
-              position="bottomRight"
-              onPress={()=>navigation.navigate('提交反馈')}
-            >
-              <Icon type="FontAwesome" name="pencil" />
-            </Fab>
-        </Container>
-    )
+export default class Home extends Component {
+    state = {
+        count: 0,
+        feedbackArr: []
+    }
+
+    componentDidMount() {
+        this.refresh = DeviceEventEmitter.addListener('refresh', (value)=>{
+            if(value) this.setState({count: 0})
+        })
+    }
+
+    componentWillUnmount() {
+        this.refresh.remove()
+    }
+
+    _renderFeedback = () => {
+        const { feedbackArr } = this.state
+        const arr = []
+        feedbackArr.forEach(value => {
+            arr.push(
+                <Feedback 
+                    description={value.description}
+                    time={value.created_at}
+                    userId={value.owner_id}
+                />
+            )
+        })
+        return arr
+    }
+
+    getFeedbacks = () => {
+        const url = '/issue/product/4ef1a91b-88f4-4d22-89a1-3d6f1b9345b1?status=opening'
+        Request(url)
+          .then(res=>this.setState({
+              count: res.result.count,
+              feedbackArr: res.result.issues
+          }))
+    }
+
+    render() {
+        const { count } = this.state
+        const { navigation } = this.props
+        console.disableYellowBox = true
+        if (count === 0) this.getFeedbacks()
+        return(
+            <Container>
+                <Content padder>
+                    <Image source={ require('../../assets/images/home.jpg') } 
+                        style={{ width: 340, height: 125, borderRadius: 10 }} 
+                    />
+                    {this._renderFeedback()}
+                </Content>
+                <Fab
+                    style={{ backgroundColor: '#5067FF' }}
+                    position="bottomRight"
+                    onPress={()=>navigation.navigate('提交反馈')}
+                >
+                    <Icon type="FontAwesome" name="pencil" />
+                </Fab>
+            </Container>
+        )
+  }  
 }
