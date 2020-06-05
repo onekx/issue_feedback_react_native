ac9d76bc77cfa3984ca8502e524427bfb75667bd
kongxin <43380516+onekx@users.noreply.github.com>
2020-06-05 21:06:03 +0800

    修复评论 bug (#24)

---
+++ src/components/CommentCard.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Text, Left, Body, Card, CardItem, Thumbnail } from 'native-base'
+import { StyleSheet, View } from 'react-native'
+
+const CommentCard = ({name, month, hours, content}) => {
+    return (
+        <Card transparent>
+            <CardItem>
+                <Left>
+                    <Thumbnail square small source={require('../images/defaultAvatar.jpg')} />
+                    <Body>
+                        <Text>{name}</Text>
+                        <Text note>{`${month}   ${hours}`}</Text>
+                    </Body>
+                </Left>
+            </CardItem>
+            <CardItem>
+                <Text style={styles.commentContent}>
+                    {content}
+                </Text>
+            </CardItem>
+            <View style={styles.divider} />
+        </Card>
+    )
+}
+
+const styles = StyleSheet.create({
+    divider: {
+        height: 1,
+        backgroundColor: '#c4c4c4'
+    },
+    commentContent: {
+        color: '#2c2c2c',
+        marginLeft: 45,
+        marginTop: -10
+    }
+})
+
+export default CommentCard
