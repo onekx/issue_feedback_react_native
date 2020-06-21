db0962f96aab75b880c30025292baf5015ffe434
onekx <1095716578@qq.com>
2020-06-21 12:52:32 +0800

    修改默认头像

---
+++ src/components/user/AvatarSource.js
@@ -0,0 +1,21 @@
+const a1 = require('../../images/a1.jpg')
+const a2 = require('../../images/a2.jpg')
+const a3 = require('../../images/a3.jpg')
+const a4 = require('../../images/a4.jpg')
+const a5 = require('../../images/a5.jpg')
+const a6 = require('../../images/a6.jpg')
+const a7 = require('../../images/a7.jpg')
+const a8 = require('../../images/a8.jpg')
+const a9 = require('../../images/a9.jpg')
+
+export default {
+    a1 : a1,
+    a2 : a2,
+    a3 : a3,
+    a4 : a4,
+    a5 : a5,
+    a6 : a6,
+    a7 : a7,
+    a8 : a8,
+    a9 : a9
+}
